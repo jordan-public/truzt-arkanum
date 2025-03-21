92c87791fc750ca0d91179f1b6c8c73e362a8a6c
jordan <jordan@bitola.com>
2025-03-21 16:50:52 -0400

    Generate random key

---
+++ cli/genkey.js
@@ -0,0 +1,10 @@
+import { Account } from "@provablehq/sdk";
+
+const account = new Account();
+const privateKey = account.privateKey();
+const viewKey = account.viewKey();
+const address = account.address();
+
+console.log("Private Key:", privateKey.to_string?.());
+console.log("View Key:", viewKey.to_string?.());
+console.log("Address:", address.to_string?.());
