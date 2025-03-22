dc2f2acca931a4e2fde9473557b1f57d71b7860d
jordan <jordan@bitola.com>
2025-03-22 03:45:10 -0400

    Typo

--- trust/transfer.sh
+++ trust/transfer.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 <wrapper_token_id>, <amount>u128 <destination> <nonce>u64"
+    echo "Usage: $0 <wrapper_token_id> <amount>u128 <destination> <nonce>u64"
     exit 1
 fi
 
